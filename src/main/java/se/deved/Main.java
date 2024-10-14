ae633843dfbcd7204f5bf1a36ec92a1c2eb6e703
EnanderW <williamenander@gmail.com>
2024-10-14 10:55:30 +0200

    init

---
+++ src/main/java/se/deved/Main.java
@@ -0,0 +1,36 @@
+package se.deved;
+
+/*
+<-----> Todo Applikation <----->
+En applikation för att schemalägga "tasks" som kan avklaras.
+
+Funktionalitet:
+1. Skapa "tasks"
+    - Schemalägga tasks
+    - Prioritera task
+    - Upprepande tasks
+    - Organisera/gruppera tasks
+2. Radera "tasks"
+3. Uppdatera "tasks" (uppdatera titel, tid)
+4. Avklara "tasks"
+5. Lista upp tasks
+    - Visa i ordning
+    - Visa tid
+    - Visa i grupper
+
+Implementation:
+- Spara "tasks" till fil
+
+- Task klass - representerar en task
+- TaskManager klass - hanterar alla tasks
+- Validation klass - utility klass för att validera information
+- CommandManager klass - hantera kommandon
+- TimeManager klass - hanterar påminnelser om schemalagda tasks
+
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
